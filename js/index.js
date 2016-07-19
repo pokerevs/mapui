d2da7104048c483e398645e6496e203f131bc9ca
Eric Betts <bettse@fastmail.fm>
2016-07-19 09:32:04 -0700

    Reindent index.js using fixed vim reindent

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@
 maptiks.trackcode='23b927ed-3ff7-43e2-9052-6b00b6d1565b';
 
 var map = new L.Map('map', {
-    fullscreenControl: true,
-    fullscreenControlOptions: {
-      position: 'topleft'
-    }
+  fullscreenControl: true,
+  fullscreenControlOptions: {
+    position: 'topleft'
+  }
 });
 var hash = new L.Hash(map);
 L.tileLayer.provider('OpenStreetMap.Mapnik', {retina: true}).addTo(map);
@@ -48,11 +48,11 @@ var autoloadOptions = {
 }
 
 var gj = L.uCluster({
-      disableClusteringAtZoom: 16
-    }, {
-      endpoint:endpoint_name, debug: (window.location.hostname == 'localhost'), usebbox: true, after: newData
-    },
-    autoloadOptions
+  disableClusteringAtZoom: 16
+}, {
+  endpoint:endpoint_name, debug: (window.location.hostname == 'localhost'), usebbox: true, after: newData
+},
+autoloadOptions
 ).addTo(map);
 
 var initialViewSet = false;
@@ -70,7 +70,7 @@ function onLocationFound(e) {
 function newData(geojson) {
   if (!initialViewSet && geojson.features.length > 0) {
     //If location data wasn't available, or was denied
-	  map.fitBounds(gj.getBounds(), {maxZoom: 16});
+    map.fitBounds(gj.getBounds(), {maxZoom: 16});
     initialViewSet = true;
   }
   showHighZoomMesasge();
@@ -95,68 +95,68 @@ function checkboxFilter(feature) {
 
 //https://gist.github.com/tmcw/3861338
 function simplestyle(f, latlon) {
-    var sizes = {
-      small: [20, 50],
-      medium: [30, 70],
-      large: [35, 90]
-    };
-    var fp = f.properties || {};
-    var size = fp['marker-size'] || 'medium';
-    var symbol = (fp['marker-symbol']) ? '-' + fp['marker-symbol'] : '';
-    var color = fp['marker-color'] || '7e7e7e';
-    color = color.replace('#', '');
-    var url = 'http://a.tiles.mapbox.com/v3/marker/' +
-          'pin-' +
-          // Internet Explorer does not support the `size[0]` syntax.
-          size.charAt(0) + symbol + '+' + color +
-          ((window.devicePixelRatio === 2) ? '@2x' : '') +
-          '.png';
+  var sizes = {
+    small: [20, 50],
+    medium: [30, 70],
+    large: [35, 90]
+  };
+  var fp = f.properties || {};
+  var size = fp['marker-size'] || 'medium';
+  var symbol = (fp['marker-symbol']) ? '-' + fp['marker-symbol'] : '';
+  var color = fp['marker-color'] || '7e7e7e';
+  color = color.replace('#', '');
+  var url = 'http://a.tiles.mapbox.com/v3/marker/' +
+    'pin-' +
+    // Internet Explorer does not support the `size[0]` syntax.
+  size.charAt(0) + symbol + '+' + color +
+    ((window.devicePixelRatio === 2) ? '@2x' : '') +
+    '.png';
 
-    var icon = new L.icon({
-        iconUrl: url,
-        iconSize: sizes[size],
-        iconAnchor: [sizes[size][0] / 2, sizes[size][1]/2],
-        popupAnchor: [-3, -sizes[size][1]/2]
-      });
+  var icon = new L.icon({
+    iconUrl: url,
+    iconSize: sizes[size],
+    iconAnchor: [sizes[size][0] / 2, sizes[size][1]/2],
+    popupAnchor: [-3, -sizes[size][1]/2]
+  });
 
-    return new L.Marker(latlon, {
-      icon: icon
-    });
+  return new L.Marker(latlon, {
+    icon: icon
+  });
 }
 
 function customStyle(f, latlon) {
-    var marker = simplestyle(f, latlon);
-    var properties = f.properties;
+  var marker = simplestyle(f, latlon);
+  var properties = f.properties;
 
-    if (properties.type == 'pokestop') {
-      if(properties.lure){
-          marker.setIcon(pokestopLureIcon);
-      } else {
-          marker.setIcon(pokestopIcon);
-      }
-    } else if (properties.type == 'wild' || properties.type == 'catchable') {
-      marker.setIcon(pokemonIcon);
-    } else if (properties.type == 'gym') {
-        if (properties.title == "Blue Gym") {
-            marker.setIcon(blueGymIcon);
-        } else if (properties.title == "Red Gym") {
-            marker.setIcon(redGymIcon);
-        } else if (properties.title == "Yellow Gym") {
-            marker.setIcon(yellowGymIcon);
-        } else {
-          marker.setIcon(neutralGymIcon);
-        }
+  if (properties.type == 'pokestop') {
+    if(properties.lure){
+      marker.setIcon(pokestopLureIcon);
+    } else {
+      marker.setIcon(pokestopIcon);
     }
+  } else if (properties.type == 'wild' || properties.type == 'catchable') {
+    marker.setIcon(pokemonIcon);
+  } else if (properties.type == 'gym') {
+    if (properties.title == "Blue Gym") {
+      marker.setIcon(blueGymIcon);
+    } else if (properties.title == "Red Gym") {
+      marker.setIcon(redGymIcon);
+    } else if (properties.title == "Yellow Gym") {
+      marker.setIcon(yellowGymIcon);
+    } else {
+      marker.setIcon(neutralGymIcon);
+    }
+  }
 
-    return marker;
+  return marker;
 }
 
 function showHighZoomMesasge(){
-    if(map.getZoom() < 10){
-        document.getElementById("highzoom").style.visibility = "initial"
-    } else {
-        document.getElementById("highzoom").style.visibility = "hidden"
-    }
+  if(map.getZoom() < 10){
+    document.getElementById("highzoom").style.visibility = "initial"
+  } else {
+    document.getElementById("highzoom").style.visibility = "hidden"
+  }
 }
 
 var GymIcon = L.Icon.extend({
